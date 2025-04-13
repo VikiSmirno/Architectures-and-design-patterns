906ab471e900e8f30e177ad1be5f1d49c7c277bb
Viktoria Smirnova <relevantforever6@gmail.com>
2025-04-14 00:01:58 +0600

    add MoveCommand

---
+++ spacewar/SpaceWar.Abstractions/MoveCommand.cs
@@ -0,0 +1,14 @@
+namespace SpaceWar.Abstractions;
+
+public class MoveCommand : ICommand
+{
+    private readonly IMovable _movable;
+    public MoveCommand(IMovable movable)
+    {
+        _movable = movable;
+    }
+    public void Execute()
+    {
+        _movable.Position += _movable.Velocity;
+    }
+}
