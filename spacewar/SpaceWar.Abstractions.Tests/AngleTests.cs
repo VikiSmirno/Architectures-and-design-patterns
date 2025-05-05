09391dbb666bde39cbecb5936154a7e71b251ef7
Viktoria Smirnova <relevantforever6@gmail.com>
2025-05-05 21:33:55 +0600

    add AngleTests

---
+++ spacewar/SpaceWar.Abstractions.Tests/AngleTests.cs
@@ -0,0 +1,81 @@
+namespace SpaceWar.Abstractions.Tests;
+
+public class AngleTests
+{
+    [Fact]
+    public void Constructor_UnitsPerCircleEqualZero_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new Angle(30, 0));
+    }
+
+    [Fact]
+    public void OperatorPlus_AngleEqualsNull_ThrowsArgumentNullException()
+    {
+        var x = new Angle(30);
+        Angle? nullAngle = null;
+
+        Assert.Throws<ArgumentNullException>(() => x + nullAngle!);
+    }
+
+    [Fact]
+    public void OperatorPlus_AddAngles_Correctly()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(30);
+
+        var sumResult = angle1 + angle2;
+
+        Assert.Equal(new Angle(60), sumResult);
+    }
+    [Fact]
+    public void OperatorPlus_AddAnglesWithDifferentUnits_ThrowsException()
+    {
+        var angle1 = new Angle(30, 360);
+        var angle2 = new Angle(30, 180);
+        var sumResult = angle1 + angle2;
+
+        Assert.Equal(new Angle(90), angle1 + angle2);
+    }
+    [Fact]
+    public void OperatorDoubleEqual_CompareAngles_EqualAngles()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(390);
+
+        Assert.True(angle1 == angle2);
+    }
+
+    [Fact]
+    public void OperatorNotEqual_CompareAngles_NotEqualAngles()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(300);
+
+        Assert.True(angle1 != angle2);
+    }
+    [Fact]
+    public void Equals_CompareAngles_Correctly()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(390);
+
+        Assert.True(angle1.Equals(angle2));
+    }
+    [Fact]
+    public void GetHashCode_CompareAngles_Correctly()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(390);
+
+        Assert.True(angle1.GetHashCode() == angle2.GetHashCode());
+    }
+    [Fact]
+    public void GetHashCode_CompareNotEqualAngles_HashDoesNotMatch()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(300);
+
+        Assert.True(angle1.GetHashCode() != angle2.GetHashCode());
+    }
+
+}
