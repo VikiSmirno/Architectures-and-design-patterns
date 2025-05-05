454f4444a958423f229ea6a61ef6b044b37634bf
Viktoria Smirnova <relevantforever6@gmail.com>
2025-05-05 17:26:50 +0600

    add RotateCommandTests

---
+++ spacewar/SpaceWar.Abstractions.Tests/RotateCommandTests.cs
@@ -0,0 +1,54 @@
+using System.Security.Cryptography;
+using Moq;
+
+namespace SpaceWar.Abstractions.Tests;
+
+public class RotateCommandTests
+{
+    [Fact]
+    public void Execute_UpdatesCurrentAngle_Correctly()
+    {
+        var rotatable = new Mock<IRotatable>();
+        rotatable.SetupGet(r => r.CurrentAngle).Returns(new Angle(45));
+        rotatable.SetupGet(r => r.RotationStep).Returns(new Angle(90));
+        var rotateCommand = new RotateCommand(rotatable.Object);
+
+        rotateCommand.Execute();
+
+        rotatable.VerifySet(r => r.CurrentAngle = new Angle(135), Times.Once);
+    }
+
+    [Fact]
+    public void Execute_UpdatesCurrentAngle_GetCurrentAngleFail()
+    {
+        var rotatable = new Mock<IRotatable>();
+        rotatable.SetupGet(r => r.CurrentAngle).Throws<Exception>();
+        rotatable.SetupGet(r => r.RotationStep).Returns(new Angle(90));
+        var rotateCommand = new RotateCommand(rotatable.Object);
+
+        Assert.Throws<Exception>(() => rotateCommand.Execute());
+    }
+
+    [Fact]
+    public void Execute_UpdatesCurrentAngle_GetRotationStepFail()
+    {
+        var rotatable = new Mock<IRotatable>();
+        rotatable.SetupGet(r => r.CurrentAngle).Returns(new Angle(45));
+        rotatable.SetupGet(r => r.RotationStep).Throws<Exception>();
+        var rotateCommand = new RotateCommand(rotatable.Object);
+
+        Assert.Throws<Exception>(() => rotateCommand.Execute());
+    }
+
+    [Fact]
+    public void Execute_UpdateCurrentAngle_ImpossibleRotateObject()
+    {
+        var rotatable = new Mock<IRotatable>();
+        rotatable.SetupGet(r => r.CurrentAngle).Returns(new Angle(45));
+        rotatable.SetupGet(r => r.RotationStep).Returns(new Angle(90));
+        rotatable.SetupSet(r => r.CurrentAngle = It.IsAny<Angle>()).Throws<InvalidOperationException>();
+        var rotateCommand = new RotateCommand(rotatable.Object);
+
+        Assert.Throws<InvalidOperationException>(() => rotateCommand.Execute());
+    }
+}
