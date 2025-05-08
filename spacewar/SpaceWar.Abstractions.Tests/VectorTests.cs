c766361ea6c7f47340d9464f8d0fa0825d538af7
Viktoria Smirnova <relevantforever6@gmail.com>
2025-05-08 22:15:14 +0600

    add VectorTests of Constructor & Iterator

--- spacewar/SpaceWar.Abstractions.Tests/VectorTests.cs
+++ spacewar/SpaceWar.Abstractions.Tests/VectorTests.cs
@@ -3,6 +3,35 @@ namespace SpaceWar.Abstractions.Tests;
 public class VectorTests
 {
     [Fact]
+    public void Constructor_CoordinatesAreNull_ThrowsArgumentNullException()
+    {
+        int[]? nullable = null;
+        Assert.Throws<ArgumentNullException>(() => new Vector(nullable!));
+    }
+
+    [Fact]
+    public void Constructor_VectorIsEmpty_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new Vector([]));
+    }
+
+    [Fact]
+    public void Iterator_GetItemByIndex_Correctly()
+    {
+        var vector = new Vector(1, 0);
+        Assert.Equal(0, vector[1]);
+    }
+
+    [Fact]
+    public void OperatorPlus_AddVectorsWithDifferentDimensions_ThrowsArgumentException()
+    {
+        var vector1 = new Vector(1, 1);
+        var vector2 = new Vector(1, 1, 1);
+
+        Assert.Throws<ArgumentException>(() => vector1 + vector2);
+    }
+
+    [Fact]
     public void OperatorPlus_AddVectors_Correctly()
     {
         var vector1 = new Vector(1, 1);
@@ -12,6 +41,7 @@ public class VectorTests
 
         Assert.Equal(new Vector(1, 0), sumResult);
     }
+
     [Fact]
     public void OperatorDoubleEqual_CompareVectors_EqualVectors()
     {
@@ -19,6 +49,15 @@ public class VectorTests
         var vector2 = new Vector(1, 1);
 
         Assert.True(vector1 == vector2);
+    }
+
+    [Fact]
+    public void OperatorDoubleEqual_CompareVectorWithNull_ReturnFalse()
+    {
+        var vector1 = new Vector(1, 1);
+        Vector? vector2 = null;
+
+        Assert.False(vector1 == vector2!);
     }
 
     [Fact]
